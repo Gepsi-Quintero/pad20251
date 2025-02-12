ce9cb87c3a168f5a6a496b6810b3a778d7ab711b
andrescallejas <andres.f.callejas@hotmail.es>
2025-02-11 21:47:09 -0500

    actividad_1 - configuracion

---
+++ src/pad_2025/actividad_1.py
@@ -0,0 +1,10 @@
+
+from clase_1 import Personas
+
+
+persona =Personas()
+persona.edad =   42
+persona.estatura = 193
+print("nombre: ",persona.nombre," edad: ",persona.edad, " cms: ", persona.estatura)
+print("nombre: ",persona.nombre," edad: ",persona.edad, " cms: ", persona.estatura)       
+        
