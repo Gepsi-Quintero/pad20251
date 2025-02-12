c0103427893fa3b36edab2c42b1df8c69706bc62
andrescallejas <andres.f.callejas@hotmail.es>
2025-02-11 20:56:27 -0500

    punto de control

--- src/pad_2025/clase_1.py
+++ src/pad_2025/clase_1.py
@@ -14,5 +14,5 @@ persona =Personas()
 persona.edad =   42
 persona.estatura = 193
 print("nombre: ",persona.nombre," edad: ",persona.edad, " cms: ", persona.estatura)
-       
+print("nombre: ",persona.nombre," edad: ",persona.edad, " cms: ", persona.estatura)       
         
