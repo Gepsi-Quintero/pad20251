012d8b1d7ec6903d23206d0edec83f1152fa564d
andrescallejas <andres.f.callejas@hotmail.es>
2025-02-18 20:16:21 -0500

    funcion lectura de archivos de texto

--- src/pad_2025/ingestiones.py
+++ src/pad_2025/ingestiones.py
@@ -14,11 +14,25 @@ class Ingestiones():
         with open(ruta_json,"r",encoding="utf-8") as f:
             datos = json.load(f)
         return datos    
+    
+    def leer_txt(self):
+        # r read w write
+
+        ruta_json = "{}txt/info.txt".format(self.ruta_static)
+        datos=""
+        with open(ruta_json,"r",encoding="utf-8") as f:
+            datos = f.read()
+        return datos   
 
 
 inges = Ingestiones() 
 datos_json = inges.leer_json()
-print(datos_json)   
+print(datos_json)
+print("************************************************************")
+print("************************************************************")
+datos_txt = inges.leer_txt()
+print(datos_txt)
+
     
     
     
