2630bdf70d4796c136b884898c2e9ec297afb4ec
andrescallejas <andres.f.callejas@hotmail.es>
2025-02-18 20:06:41 -0500

    clase ingestiones funcion json

---
+++ src/pad_2025/ingestiones.py
@@ -0,0 +1,24 @@
+import json
+
+
+
+class Ingestiones():
+    def __init__(self):
+        self.ruta_static="D:/UIDigital/PAD_2025101/repositorios/pad20251/src/pad_2025/static/"
+        
+    def leer_json(self):
+        # r read w write
+
+        ruta_json = "{}json/datos_persona.json".format(self.ruta_static)
+        datos=""
+        with open(ruta_json,"r",encoding="utf-8") as f:
+            datos = json.load(f)
+        return datos    
+
+
+inges = Ingestiones() 
+datos_json = inges.leer_json()
+print(datos_json)   
+    
+    
+    
